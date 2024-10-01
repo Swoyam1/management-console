aa4bc608d8d4d43773138170d47b69afa9e4d56a
Swoyam Pant <44669396+Swoyam1@users.noreply.github.com>
2024-10-01 22:33:28 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: process.env.REACT_APP_API_URL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://management-console-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
